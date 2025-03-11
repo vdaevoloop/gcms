b64a1b655030589f691dfcf41f52c3a3a14d8ffc
Viet Duc Do <duc.do32@mailbox.org>
2025-03-11 21:03:10 +0100

    read mzml

--- pyopenms_client/PyOpenMsClient.py
+++ pyopenms_client/PyOpenMsClient.py
@@ -1,4 +1,5 @@
 import pyopenms as oms
+import logging
 import pathlib
 
 
@@ -6,25 +7,37 @@ class Exp:
     """Adapter to MSExperiment to read and write mzML files.
     Argument:
         mzml_file: Must be the absolute path to the file.
-
     """
 
-    CLASS_TESTDATA= ".data/test_mzml/PS_R667_EST_3.mzML"
+    TESTDATA = ".data/test_mzml/PS_R667_EST_3.mzML"
 
-
-    def __init__(self, mzml_file: str|None =None, testdata:bool=True):
+    def __init__(
+        self, mzml_file: str | None = None, testdata: bool = True, selfinit: bool = True
+    ) -> None:
         self.exp = oms.MSExperiment()
+
         if mzml_file is None and testdata is True:
-            self.mzml_file = pathlib.Path(CLASS_TESTDATA)
+            self.mzml_file = pathlib.Path(TESTDATA)
+        elif mzml_file is None and testdata is False:
+            self.mzml_file = None
+        else:
+            self.mzml_file = mzml_file
 
+        if self.mzml_file is not None and selfinit is True:
+            self.set_dataset(str(self.mzml_file))
 
-    def import_mzml(self, file) -> None:
+        return
+
+    def set_dataset(self, file: str) -> None:
         """Reads a mzML file into an Exp object"""
-        oms.MzMLFile().load(file, self.exp)
+        try:
+            oms.MzMLFile().load(file, self.exp)
+        except Exception as e:
+            logging.error(f"Error while importing mzML file in Exp.set_dataset: {e}")
 
-    def extract_chrom(self) -> oms.MSChromatogram
+    def extract_chrom(self) -> oms.MSChromatogram:
         """Extracts a TIC (total intensity current)."""
-
+        return oms.MSChromatogram()
 
 
 class Chrom:
@@ -34,5 +47,3 @@ class Chrom:
 
     def __init__(self, mzml_file=None) -> None:
         self.chrom = None
-        self.exp = 
-
