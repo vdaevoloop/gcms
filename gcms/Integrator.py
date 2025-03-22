3593bdd9c8d3fe3fd94dae30b05daa54f684a31b
Viet Duc Do <duc.do32@mailbox.org>
2025-03-22 10:25:58 +0100

    pre check colums in TrapezoidIntegrator

---
+++ gcms/Integrator.py
@@ -0,0 +1,35 @@
+from abc import ABC, abstractmethod
+import pandas as pd
+
+
+class ChromIntegrator(ABC):
+    """Interface for peak area integrator"""
+
+    @abstractmethod
+    def integrate(self, chrom: pd.Series | pd.DataFrame, peaks: pd.DataFrame):
+        """Integrates the area between left and right borders of a peak
+        Args:
+            chrom: Chromatogram data as a pandas Series or DataFrame. Must include column 'intensity'
+            peaks: Peaks data ad Series or DataFrame. Must include peak borders
+
+        Returns:
+            None. Integrals are added to peaks object.
+
+        Raises:
+            ValueError: If peak borders are not included.
+                        If intensity is not included in chrom.
+        """
+        pass
+
+
+class ChromTrapezoidIntegrator(ChromIntegrator):
+    """Using trapezoid method to calculate are beneath peaks"""
+
+    def integrate(self, chrom: pd.Series | pd.DataFrame, peaks: pd.DataFrame):
+        if "intensity" not in chrom.columns:
+            raise ValueError(
+                "Error integrating peak area: chromatogram does not contain intensity column"
+            )
+        to_check = ["left_border", "right_border"]
+        if not all(value in peaks.columns for value in to_check):
+            raise ValueError("error integrating peak area: peak borders are missing")
